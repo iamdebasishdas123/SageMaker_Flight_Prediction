2aa7c4a9077c3fd6f0ae1eb0b18b781bde425b76
Debasish Das <146256089+iamdebasishdas123@users.noreply.github.com>
2024-07-01 14:39:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -259,7 +259,7 @@ preprocessor = Pipeline(steps=[
 ])
 
 # read the training data
-train = pd.read_csv(r"C:\Users\Debasish Das\Documents\01_ML Project\Ml Projects\Flight_Price_Prediction\Dataset\train.csv")
+train = pd.read_csv("train.csv")
 X_train = train.drop(columns="price")
 y_train = train.price.copy()
 
